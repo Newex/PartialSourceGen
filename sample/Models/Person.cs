9918659b30f48d343c91dd072d0b82e4badfc72f
Johnny Kristian Moeberg Sørensen <mojo11ku@hotmail.com>
2024-03-31 05:01:26 -0100

    wip: Add xml documentation to sample model

--- sample/Models/Person.cs
+++ sample/Models/Person.cs
@@ -13,6 +13,14 @@ public readonly record struct Person
     }
 
     public required int ID { get; init; }
+
+    /// <summary>
+    /// The first name
+    /// </summary>
     public string FirstName { get; init; } = string.Empty;
+
+    /// <summary>
+    /// The last name
+    /// </summary>
     public string? LastName { get; init; }
 }
