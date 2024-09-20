5aff8392e89d2b2d7d482873c62eb089cdf70e88
Newex <johnnykristianmoebergsoerensen@gmail.com>
2024-09-20 22:44:58 -0100

    wip: Aliasing required attribute in sample

--- sample/Models/Person.cs
+++ sample/Models/Person.cs
@@ -1,6 +1,5 @@
 ﻿using PartialSourceGen;
-using System;
-using System.ComponentModel.DataAnnotations;
+using Important = System.ComponentModel.DataAnnotations.RequiredAttribute;
 
 namespace Sample.Models;
 
@@ -15,7 +14,7 @@ public readonly partial record struct Person<T>
     {
     }
 
-    [Required]
+    [Important]
     public int ID { get; init; } = 123;
 
     /// <summary>
